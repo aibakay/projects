e718f828a6d2137a8e2e3a9f14aa304de66848f4
aibakay <a.baakaay@gmail.com>
2024-10-29 03:12:16 +0300

    +казино

--- index.py
+++ index.py
@@ -1,3 +1,31 @@
-print('Как тебя зовут?')
-name = input('Введите имя \n')
-print(f"Привет, {name}")
+number = int(input())
+if 0 <= number <= 36:
+    if number == 0:
+        print("зеленый")
+    elif 0 < number < 11:
+        if number % 2 == 0:
+            print("черный")
+        else:
+            print("красный")
+    elif 10 < number < 19:
+        if number % 2 == 0:
+            print("красный")
+        else:
+            print("черный")
+    elif 18 < number < 29:
+        if number % 2 == 0:
+            print("черный")
+        else:
+            print("красный")
+    elif 28 < number < 37:
+        if number % 2 == 0:
+            print("красный")
+        else:
+            print("черный")
+else:
+    print("ошибка ввода")
+
+
+
+
+
